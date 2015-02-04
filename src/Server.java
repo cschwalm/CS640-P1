294ada76b85afb30e8df76cf41dd94dc794e708e
Corbin Schwalm <schwalm@wisc.edu>
2015-02-04 11:38:09 -0600

    Almost finished server code :

---
+++ src/Server.java
@@ -0,0 +1,50 @@
+import java.io.*;
+import java.net.*;
+
+
+public class Server {
+	
+	public final int PACKET_LENGTH = 1000;
+	
+	public void processClientData(int portNum) {
+		
+		if (portNum < 1024 || portNum > 65535)
+			throw new IllegalArgumentException("Invalid Port Number");
+		
+		Socket client;
+		int bytesRead = 0, bytesReadTotal = 0;
+		byte[] dataReceived = new byte[PACKET_LENGTH];
+		long timeStart, timeEnd;
+		
+		try {
+		
+			ServerSocket soc = new ServerSocket(portNum);
+			
+			if ((client = soc.accept()) != null) {
+				
+				InputStream data = client.getInputStream();
+				timeStart = System.currentTimeMillis();
+				
+				while (bytesRead != -1) {
+				
+					bytesRead = data.read(dataReceived, 0, PACKET_LENGTH);
+					bytesReadTotal += bytesRead;
+				}
+				
+				timeEnd = System.currentTimeMillis();
+				
+				client.close();
+				soc.close();		
+			}		
+		
+		} catch (IOException ex) {
+			
+			Iperfer.handleError("IO Error Occurred. Exiting...");
+		}
+		
+		
+		
+		
+	}
+
+}
